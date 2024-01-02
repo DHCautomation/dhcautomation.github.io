878b8f76689b9b73f639d591ddffe41e7a57e14e
DHCautomation <121466319+DHCautomation@users.noreply.github.com>
2024-01-02 12:19:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // script.js
 
-const client = new Paho.MQTT.Client( ${{ secrets.MQTT_SERVER }}, 8083, "web_" + parseInt(Math.random() * 100, 10));
+// Load dotenv only if running locally (not in GitHub Actions)
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const client = new Paho.MQTT.Client(process.env.GITHUB_SECRET_MQTT_SERVER || process.env.MQTT_SERVER || "fallback_server_address", 8083, "web_" + parseInt(Math.random() * 100, 10));
 
 client.onConnectionLost = function (responseObject) {
     if (responseObject.errorCode !== 0) {
@@ -19,8 +24,8 @@ const connectOptions = {
         client.subscribe("your/mqtt/topic");
     },
     useSSL: true,
-    userName: MQTT_USERNAME: ${{ secrets.MQTT_USERNAME }},
-    password: MQTT_PASSWORD: ${{ secrets.MQTT_PASSWORD }},
+    userName: process.env.GITHUB_SECRET_MQTT_USERNAME || process.env.MQTT_USERNAME,
+    password: process.env.GITHUB_SECRET_MQTT_PASSWORD || process.env.MQTT_PASSWORD,
 };
 
 client.connect(connectOptions);
